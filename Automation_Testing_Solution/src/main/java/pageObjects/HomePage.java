0207c1c418a7c4b898d09e86603200a14dcdd0f1
olarcik <vladis.olaru@gmail.com>
2023-03-27 16:55:15 +0300

    PageObject Pattern commit

---
+++ Automation_Testing_Solution/src/main/java/pageObjects/HomePage.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends Page {
+
+    public HomePage(WebDriver driver){
+
+        super(driver);
+
+    }
+}
---
+++ Automation_Testing_Solution/src/main/java/pageObjects/RegisterPage.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPage extends Page {
+
+    public RegisterPage(WebDriver driver){
+        super(driver);
+    }
+
+    @FindBy(id = "input-firstname" )
+    private WebElement firstNameInput;
+    @FindBy(id = "input-lastname" )
+    private WebElement lastNameInput;
+    @FindBy(id = "input-email" )
+    private WebElement emailInput;
+    @FindBy(id = "input-password")
+    private WebElement passwordInput;
+    @FindBy(xpath = "//input[@name=\"agree\"]")
+    private WebElement privaceCheckBox;
+    @FindBy(xpath = "//button[@type=\"submit\"]" )
+    private WebElement continueButton;
+
+    public void fillInTheRegisterPage(String firstName, String lastName, String email, String password) {
+
+        firstNameInput.sendKeys(firstName);
+        lastNameInput.sendKeys(lastName);
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+
+    }
+
+
+
+
+
+
+}
