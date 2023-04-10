018c3e4cff8d86a3c7b5fec93de451230d034766
olarcik <vladis.olaru@gmail.com>
2023-04-10 23:44:29 +0300

    RegisterFlowFeature commit

--- Automation_Testing_Solution/src/main/java/stepDefinition/GeneralSteps.java
+++ Automation_Testing_Solution/src/main/java/stepDefinition/GeneralSteps.java
@@ -3,6 +3,8 @@ package stepDefinition;
 import contextManagers.TestContext;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.junit.jupiter.api.Assertions;
 
 public class GeneralSteps {
 
@@ -14,5 +16,11 @@ public class GeneralSteps {
     @Given("^\"([^\"]*)\" is accesed$")
     public void isAccesed(String adresaURL)  {
         testContext.getWebDriverManager().getDriver().get(adresaURL);
+    }
+
+    @Then("^\"([^\"]*)\" is present within the current url$")
+    public void isPresentWithinTheCurrentUrl(String urlKeyWord){
+        Assertions.assertTrue(testContext.getWebDriverManager().getDriver().getCurrentUrl().contains(urlKeyWord));
+
     }
 }
